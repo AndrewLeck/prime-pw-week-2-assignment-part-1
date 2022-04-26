0b10595dbf819a8a6e96808fcac2bc42b96224e7
Andrew Leck <Aleck360@gmail.com>
2022-04-26 15:54:28 -0500

    fixed question 15

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -58,16 +58,26 @@ if( luckyNumber === 2 && adventurous ){
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
-if( pets < allowedPets){
-    console.log('I can have more pets');
-    elseif(pets === allowedPets)}
-        console.log('I have enough pets');
-        elseif( pets > allowedPets){
-            console.log('Oh no, I have too many pets!');
-        }
-    }   
-}
 
+//if(pets < allowedPets){
+  //  console.log('I have can have more pets!');
+    //     if( pets === allowedPets){
+      //      console.log("I have enough pets");
+        //         if( pets > allowedPets){
+          //          console.log('Oh no, i have too many pets!');
+            //    }
+        //}
+//}
+
+if( pets < allowedPets){
+    console.log('I can have more pets!');
+}
+else if( pets === allowedPets){
+    console.log(' I have enough pets');
+}
+else if ( pets > allowedPets){
+    console.log(' Oh no, i have too many pets!');
+}
 
 // STRETCH GOALS:
 
@@ -76,6 +86,9 @@ if( pets < allowedPets){
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
+
+
+
 
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1

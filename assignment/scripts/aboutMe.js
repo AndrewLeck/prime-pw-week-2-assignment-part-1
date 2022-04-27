6a52e3020d9935bb7dc3413232d8595fba2fa886
Andrew Leck <Aleck360@gmail.com>
2022-04-26 23:08:47 -0500

    completed question 17

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -100,6 +100,28 @@ let mostPets;
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 
+const expr ='3';
+switch (expr) {
+  case '1':
+    console.log('"First is the worst" if your lucky number is 1');
+    break;
+  case '2':
+    console.log('"Second is the best" if your lucky number is 2');
+    break;
+  case '3':
+    console.log('"Third is the one with the polka dot dress" if your lucky number is 3');
+    break;
+  default:
+    console.log('"Luck is what happens when preparation meets opportunity"');
+}
+
+
+
+
+
+
+
+
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
 // Create a conditional: if adventurous is true, console log "Adventures are great!", 
